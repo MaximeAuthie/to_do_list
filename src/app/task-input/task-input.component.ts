47531333faf703486bf6ae3637e458e789d3312f
Maxime Authié <authie.maxime@orange.fr>
2023-05-14 19:31:12 +0200

    Ajout composant task-input

---
+++ src/app/task-input/task-input.component.ts
@@ -0,0 +1,18 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task-input',
+  templateUrl: './task-input.component.html',
+  styleUrls: ['./task-input.component.css']
+})
+export class TaskInputComponent {
+
+  //!Déclaration de mon Emit : pour remonter un objet vers le composant parent TaskList
+  @Output() taskCreated = new EventEmitter<{taskTitle:string, taskDescription:string}>();
+  title:string = '';
+  description:string = '';
+
+  createNewTask() {
+    this.taskCreated.emit({taskTitle : this.title, taskDescription: this.description});
+  }
+}
