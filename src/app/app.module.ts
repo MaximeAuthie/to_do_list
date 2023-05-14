ae8dc047416fffab4513512fbdca262596f45eb3
Maxime Authié <authie.maxime@orange.fr>
2023-05-14 19:34:44 +0200

    Ajout librairie pour api + route tasks-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 //! Import de la librairie router
 import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router'
+
+//! Import de la librairie pour utiliser des API
+import { HttpClientModule } from '@angular/common/http';
 
 //! Import des composants
 import { AppComponent } from './app.component';
@@ -11,13 +15,14 @@ import { HomeComponent } from './home/home.component';
 import { ListsListComponent } from './lists-list/lists-list.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
 import { UserAccountComponent } from './user-account/user-account.component';
-import { FormsModule } from '@angular/forms';
 import { WeatherComponent } from './weather/weather.component';
+import { TaskInputComponent } from './task-input/task-input.component';
 
 //! Déclaration des routes du projet
 const appRoutes:Routes = [
   {path:'', component: HomeComponent},
   {path:'account', component: UserAccountComponent},
+  {path:'tasks-list/:id', component: TasksListComponent},
 ]
 
 @NgModule({
@@ -27,12 +32,14 @@ const appRoutes:Routes = [
     ListsListComponent,
     TasksListComponent,
     UserAccountComponent,
-    WeatherComponent
+    WeatherComponent,
+    TaskInputComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
