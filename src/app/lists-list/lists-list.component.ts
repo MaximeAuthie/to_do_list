1b4d91162e79c34b362fbc9281b1ef7f2c7aefda
Maxime Authié <authie.maxime@orange.fr>
2023-05-15 21:43:15 +0200

    Typage et modification des variables

--- src/app/lists-list/lists-list.component.ts
+++ src/app/lists-list/lists-list.component.ts
@@ -1,47 +1,67 @@
 import { Component } from '@angular/core';
 
+interface List {
+    userId:number;
+    listId:number;
+    title:string;
+    tasksNumber:number;
+    checkedTasksNumber :number;
+}
+
 @Component({
   selector: 'app-lists-list',
   templateUrl: './lists-list.component.html',
   styleUrls: ['./lists-list.component.css']
 })
+
 export class ListsListComponent {
- lists = [
+ lists:Array<List>=[];
+
+  ngOnInit() {
+    this.lists = [
   {
     userId: 1,
     listId: 1,
     title: 'Courses',
-    tasksNumber: 37
+    tasksNumber: 4,
+    checkedTasksNumber : 1
   },
   {
     userId: 1,
     listId: 2,
     title: 'Fournitures',
-    tasksNumber: 13
+    tasksNumber: 13,
+    checkedTasksNumber : 1
   },
   {
     userId: 1,
     listId: 3,
     title: 'Vacances',
-    tasksNumber: 28
+    tasksNumber: 28,
+    checkedTasksNumber : 1
   },
   {
     userId: 1,
     listId: 4,
     title: 'Projet ToDoList',
-    tasksNumber: 7
+    tasksNumber: 7,
+    checkedTasksNumber : 1
   },
   {
     userId: 1,
     listId: 5,
     title: 'Projet Brut Messenger',
-    tasksNumber: 14
+    tasksNumber: 14,
+    checkedTasksNumber : 1
   },
   {
     userId: 1,
     listId: 6,
     title: 'Bricolage',
-    tasksNumber: 5
+    tasksNumber: 5,
+    checkedTasksNumber : 1
   }
  ]
+  }
+
 }
