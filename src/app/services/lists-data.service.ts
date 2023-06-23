68560688dd1cfff98e0395274454606689659430
Maxime Authié <authie.maxime@orange.fr>
2023-06-23 18:54:51 +0200

    Push d'une nouvelle liste dans un User

--- src/app/services/lists-data.service.ts
+++ src/app/services/lists-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Database, getDatabase, ref, set } from "firebase/database";
+import { Database, getDatabase, ref, set, update, push } from "firebase/database";
 import { List } from '../lists-list/lists-list.component';
 
 @Injectable({
@@ -9,12 +9,14 @@ export class ListsDataService {
 
   addList(list: List) {
     const db: Database = getDatabase();
-    set(ref(db, 'lists/'), {user: list.userId})
-    set(ref(db, 'lists/user:' + list.userId + '/' + list.listId), {
-      title: list.title,
-      userId: list.userId,
-      tasksNumber:list.tasksNumber,
-      checkedTasksNumber : list.checkedTasksNumber
+    const postListRef = ref(db, 'lists/user:' + list.userId);
+    const newPostRef = push(postListRef);
+    set(newPostRef, {
+        title: list.title,
+        description: list.description,
+        userId: list.userId,
+        tasksNumber:list.tasksNumber,
+        checkedTasksNumber : list.checkedTasksNumber
     })
   }
 }
