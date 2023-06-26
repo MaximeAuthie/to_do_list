504a254d8c2eb2b5ebd601dec8a7b1ea35b18b32
Maxime Authié <authie.maxime@orange.fr>
2023-06-26 21:29:05 +0200

    Finalisation ajout liste

--- src/app/list-input/list-input.component.ts
+++ src/app/list-input/list-input.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ListsDataService } from '../services/lists-data.service';
 import { List } from '../lists-list/lists-list.component';
+import { __values } from 'tslib';
 
 @Component({
   selector:     'app-list-input',
@@ -11,11 +12,16 @@ import { List } from '../lists-list/lists-list.component';
 
 export class ListInputComponent {
 
+  //! Générer une instance de ListsDataService
   constructor(private _listDataService: ListsDataService) {}
+
+  //! Définir les variables nécessaires
   title: string       = '';
   description:string  = '';
 
+  //! Méthode pour créer une nouvelle liste
   createNewList() {
+    //! Créer un objet de type List
     const newList: List = {
       userId:             1,
       title:              this.title,
@@ -24,7 +30,16 @@ export class ListInputComponent {
       checkedTasksNumber: 0
     }
 
-    this._listDataService.addList(newList);
+    //! Appeller la méthode addList du service listDataService
+    this._listDataService.addList(newList)
+    .then(() => {
+      console.log('List added successfully');
+    })
+    .catch((error) => {
+      console.log('Unsuccessfull operation : ' + error);
+    })
+
+    //! Remettre les champs du formulaire de création de liste à 0
     this.title        = '';
     this.description  = '';
   }
