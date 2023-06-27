fffd81f869d4cc63f457d516fbf5c1978948c2e6
Maxime Authié <authie.maxime@orange.fr>
2023-06-27 18:44:30 +0200

    Modif interface list + interface importedList

--- src/app/lists-list/lists-list.component.ts
+++ src/app/lists-list/lists-list.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { ListsDataService } from '../services/lists-data.service';
+import { getDatabase, ref, onValue, DataSnapshot} from "firebase/database";
 
 export interface List {
-    userId:number;
+    userId:string;
     title:string;
     description:string;
     tasksNumber:number;
     checkedTasksNumber :number;
+}
+
+interface importedList {
+  listId:string | null;
+  userId:string;
+  title:string;
+  description:string;
+  tasksNumber:number;
+  checkedTasksNumber :number;
 }
 
 @Component({
@@ -15,53 +26,40 @@ export interface List {
 })
 
 export class ListsListComponent {
- lists:Array<List>=[];
+
+  constructor(private _listDataService: ListsDataService) {};
+ lists: Array<importedList>=[];
+
+ //! Méthodes
+ getAllLists(snapshot: DataSnapshot) {
+  // this._listDataService.readLists('a1df5f4f4g4ede5de5d4azd89');
+  let _listArray: Array<importedList> = [];
+
+  snapshot.forEach((aList) => {
+    let newList: importedList = aList.val();
+
+    _listArray.push({
+      listId: aList.key,
+      userId: newList.userId,
+      title: newList.title,
+      description: newList.description,
+      tasksNumber: newList.tasksNumber,
+      checkedTasksNumber : newList.checkedTasksNumber
+    });
+    console.log(aList.key);
+
+  });
+
+  this.lists = _listArray;
+  console.log(snapshot);
+
+  console.log(this.lists);
+
+ }
 
   ngOnInit() {
-    this.lists = [
-  {
-    userId: 1,
-    title: 'Courses',
-    description: '',
-    tasksNumber: 4,
-    checkedTasksNumber : 1
-  },
-  {
-    userId: 1,
-    title: 'Fournitures',
-    description: '',
-    tasksNumber: 13,
-    checkedTasksNumber : 1
-  },
-  {
-    userId: 1,
-    title: 'Vacances',
-    description: '',
-    tasksNumber: 28,
-    checkedTasksNumber : 1
-  },
-  {
-    userId: 1,
-    title: 'Projet ToDoList',
-    description: '',
-    tasksNumber: 7,
-    checkedTasksNumber : 1
-  },
-  {
-    userId: 1,
-    title: 'Projet Brut Messenger',
-    description: '',
-    tasksNumber: 14,
-    checkedTasksNumber : 1
-  },
-  {
-    userId: 1,
-    title: 'Bricolage',
-    description: '',
-    tasksNumber: 5,
-    checkedTasksNumber : 1
+    const db = getDatabase();
+    const directory = ref(db, 'listsByUser/a1df5f4f4g4ede5de5d4azd89');
+    onValue(directory, (snapshot) => this.getAllLists(snapshot));
   }
- ]
-  }
-
 }
