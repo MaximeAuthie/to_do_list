1cd0d52bb81c0a0fecd6c3b773c575461f068751
Maxime Authié <authie.maxime@orange.fr>
2023-06-29 18:28:07 +0200

    Ajout méthhode getAllTasks()

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { List } from '../lists-list/lists-list.component';
 import { getDatabase, ref, onValue, DataSnapshot} from "firebase/database";
+import { ListsDataService } from '../services/lists-data.service';
 
 //! Interfaces permettant de définir un typage pour les variable de la classe TasksListComponent
 export interface Task {
@@ -10,6 +11,12 @@ export interface Task {
   isCheck: boolean;
 }
 
+interface importedTask {
+  taskId: string | null;
+  title: string;
+  description: string;
+  isCheck: boolean;
+}
 
 @Component({
   selector: 'app-tasks-list',
@@ -39,17 +46,39 @@ export class TasksListComponent {
 
   progressRate: string = '';
 
-  constructor(private route:ActivatedRoute) {}
-  getAllTacks(snapshot: DataSnapshot) {
-    //! CODER LA FONCTION
+  constructor(private route:ActivatedRoute, private _listDataService: ListsDataService ) {}
+  getAllTasks(snapshot: DataSnapshot) {
+
+    let _tasksArray: Array<importedTask> = [];
+
+    snapshot.forEach((aTask) => {
+      let newTask: importedTask = aTask.val();
+
+      _tasksArray.push({
+        taskId:       aTask.key,
+        title:        newTask.title,
+        description:  newTask.description,
+        isCheck:      newTask.isCheck
+      });
+      console.log(aTask.key);
+    })
+
+    this.tasks = _tasksArray;
+    console.log(this._listDataService.getList('a1df5f4f4g4ede5de5d4azd89', this.listId));
+
   }
   ngOnInit() {
-    //Récupération de l'ID de la lsite depuis la route
+    //Récupération de l'ID de la liste depuis la route
     this.listId = this.route.snapshot.params['id'];
 
+    //Démarrade la surveillance des modification des tâches de la liste dans la BDD
     const db = getDatabase();
-    const directory = ref(db, 'listsByUser/a1df5f4f4g4ede5de5d4azd89');
+    const directory = ref(db, 'tasksByList/' + this.listId);
     onValue(directory, (snapshot) => this.getAllTasks(snapshot));
+
+    //Récupération des informations de la liste pour les afficher dans le templa
+    let returnedList:any = this._listDataService.getList('a1df5f4f4g4ede5de5d4azd89', this.listId)
+    console.log(returnedList);
 
     this.progressRate = this.progressRateCalculation() + '%';
 
